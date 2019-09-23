6fe237138c214a223883cf2a98be7e6771f959e9
Robert Holland <robertrhollandiii@gmail.com>
2019-09-23 07:27:52 -0400

    3/4 Exercises Completed. Dec. to Binary 1/2 completed. Need to finish portion to read and print out correct numbers. Conversion equation working.

--- module-1/05_Command_Line_Programs/student-exercise/DecimalToBinary/Program.cs
+++ module-1/05_Command_Line_Programs/student-exercise/DecimalToBinary/Program.cs
@@ -9,13 +9,26 @@ namespace DecimalToBinary
             Console.WriteLine("Please enter in a series of decimal values (separated by spaces): ");
             string valuesString = Console.ReadLine();
             string[] values = valuesString.Split(" ");
+            int intVal = int.Parse(valuesString);
             int i;
 
-            for (i = 0; i < values.Length; i++)
+            //for (i = 0; i < values.Length; i++)
+            //{
+            //    int intValues = int.Parse(values[i]);
+            //    Console.WriteLine($"{intValues % 2}{((intValues / 2) % 2)}" );
+            //}
+
+            string strVal = ("");
+
+            while (intVal != 0)
             {
-                int intValues = int.Parse(values[i]);
-                Console.WriteLine(intValues / 2);
+                strVal = ($"{intVal % 2}{strVal}");
+                intVal = intVal / 2;
             }
+
+
+
+            Console.WriteLine(strVal);
             Console.ReadLine();
         }
     }
