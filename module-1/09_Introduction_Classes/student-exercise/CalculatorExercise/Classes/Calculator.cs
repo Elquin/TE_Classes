cc89eff74e991a63c9cc0b6b806348a8b1a3a864
Robert Holland <robertrhollandiii@gmail.com>
2019-09-27 08:08:51 -0400

    Completed Module 1 Day 8

--- module-1/09_Introduction_Classes/student-exercise/CalculatorExercise/Classes/Calculator.cs
+++ module-1/09_Introduction_Classes/student-exercise/CalculatorExercise/Classes/Calculator.cs
@@ -11,7 +11,45 @@ namespace TechElevator.Classes
     /// </summary>
     public class Calculator
     {
-        
 
+        //constructor
+        public Calculator()
+        {
+            int Result = 0;
+        }
+
+        //Class properties
+        public int Result { get; private set; }
+
+        //Methods
+        public int Add(int addend)
+        {
+            Result = addend + Result;
+            return Result;
+        }
+
+        public int Multiply(int multiplier)
+        {
+            Result = Result * multiplier;
+            return Result;
+
+        }
+
+        public int Power(int exponent)
+        {
+            Result = (int)Math.Pow(Result, exponent);
+            return Result;
+        }
+
+        public void Reset()
+        {
+            Result = 0;
+        }
+
+        public int Subtract(int subtrahend)
+        {
+            Result = Result - subtrahend;
+            return Result;
+        }
     }
 }
