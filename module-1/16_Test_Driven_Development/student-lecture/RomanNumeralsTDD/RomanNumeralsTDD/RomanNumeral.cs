a684eac664288950f758f8b6b49173a8127b6c18
Robert Holland <robertrhollandiii@gmail.com>
2019-10-07 11:04:40 -0400

    Done with step 3 of TDD exercise

---
+++ module-1/16_Test_Driven_Development/student-lecture/RomanNumeralsTDD/RomanNumeralsTDD/RomanNumeral.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace RomanNumeralsTDD
+{
+    public class RomanNumeral
+    {
+        public string ToRoman(int number)
+        {
+            switch (number)
+            {
+                case 1:
+                    return "I";
+                case 5:
+                    return "V";
+                case 10:
+                    return "X";
+                case 50:
+                    return "L";
+                case 100:
+                    return "C";
+                case 500:
+                    return "D";
+                case 1000:
+                    return "M";
+                default:
+                    return "";
+            }
+        }
+    }
+}
