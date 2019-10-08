dd32a5befc8b88bfc0e9f76f87ed7c7e61b0d5b6
Robert Holland <robertrhollandiii@gmail.com>
2019-10-08 07:14:20 -0400

    Pushing KataRomanNumerals

--- module-1/16_Test_Driven_Development/student-exercise/Exercises.Tests/KataRomanNumeralsTests.cs
+++ module-1/16_Test_Driven_Development/student-exercise/Exercises.Tests/KataRomanNumeralsTests.cs
@@ -6,6 +6,67 @@ namespace Exercises.Tests
     [TestClass]
     public class KataRomanNumeralsTests
     {
-        
+        [DataTestMethod]
+        [DataRow(1, "I")]
+        [DataRow(5, "V")]
+        [DataRow(10, "X")]
+        [DataRow(50, "L")]
+        [DataRow(100, "C")]
+        [DataRow(500, "D")]
+        [DataRow(1000, "M")]
+        [DataRow(2, "II")]
+        [DataRow(3, "III")]
+        [DataRow(200, "CC")]
+        [DataRow(3000, "MMM")]
+        [DataRow(6, "VI")]
+        [DataRow(12, "XII")]
+        [DataRow(3300, "MMMCCC")]
+        [DataRow(3333, "MMMCCCXXXIII")]
+        [DataRow(4, "IV")]
+        [DataRow(1994, "MCMXCIV")]
+        public void TestRomanNumeral(int numberToTest, string expectedResult)
+        {
+            //Arrange
+            // Create a new RomanNumeral object
+            KataRomanNumerals roman = new KataRomanNumerals();
+
+            //Act
+            string actualRN = roman.ToRoman(numberToTest);
+
+            //Assert
+            Assert.AreEqual(expectedResult, actualRN);
+
+        }
+        #region
+        //[TestMethod]
+        //public void TestRomanNumeralForOne()
+        //{
+        //    //Arrange
+        //    // Create a new RomanNumeral object
+        //    RomanNumeral roman = new RomanNumeral();
+
+        //    //Act
+        //    string actualRN = roman.ToRoman(1);
+
+        //    //Assert
+        //    Assert.AreEqual("I", actualRN);
+
+        //}
+
+        //[TestMethod]
+        //public void TestRomanNumeralForFive()
+        //{
+        //    //Arrange
+        //    // Create a new RomanNumeral object
+        //    RomanNumeral roman = new RomanNumeral();
+
+        //    //Act
+        //    string actualRN = roman.ToRoman(5);
+
+        //    //Assert
+        //    Assert.AreEqual("V", actualRN);
+
+        //}
+        #endregion
     }
 }
--- module-1/16_Test_Driven_Development/student-exercise/Exercises/KataRomanNumerals.cs
+++ module-1/16_Test_Driven_Development/student-exercise/Exercises/KataRomanNumerals.cs
@@ -8,5 +8,67 @@ namespace Exercises
 {
     public class KataRomanNumerals
     {
+        public string ToRoman(int number)
+        {
+
+            Dictionary<int, string> numerals = new Dictionary<int, string>()
+            {
+                {1000, "M" },
+                {900, "CM" },
+                {500, "D" },
+                {400, "CD" },
+                {100, "C" },
+                {90, "XC" },
+                {50, "L" },
+                {40, "XL" },
+                {10, "X" },
+                {9, "IX" },
+                {5, "V" },
+                {4, "IV" },
+                {1, "I" },
+            };
+
+            string result = "";
+
+            foreach (int key in numerals.Keys)
+            {
+                while (number >= key)
+                {
+                    result += numerals[key];
+                    number -= key;
+                }
+            }
+
+            #region
+            //switch (number)
+            //{
+            //    case 1:
+            //        result += "I";
+            //        number -= 1;
+            //        break;
+            //    case 5:
+            //        result += "V";
+            //        break;
+            //    case 10:
+            //        result += "X";
+            //        break;
+            //    case 50:
+            //        result += "L";
+            //        break;
+            //    case 100:
+            //        result += "C";
+            //        break;
+            //    case 500:
+            //        result += "D";
+            //        break;
+            //    case 1000:
+            //        result += "M";
+            //        break;
+            //}
+            #endregion
+
+            return result;
+
+        }
     }
 }
