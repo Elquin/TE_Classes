c76de1731b772161004a59ccb26b6db5ca294b96
Robert Holland <robertrhollandiii@gmail.com>
2019-10-09 19:56:21 -0400

    Mod 1 Day 18 Exercise for FizzWriter Completed.

--- module-1/18_FileIO_Writing_out/student-exercise/FizzWriter/Program.cs
+++ module-1/18_FileIO_Writing_out/student-exercise/FizzWriter/Program.cs
@@ -5,17 +5,38 @@ namespace FizzWriter
 {
     class Program
     {
+        private static string GetFizzNumber(int number)
+        {
+            if (number % 3 == 0 && number % 5 == 0)
+            {
+                return "FizzBuzz";
+            }
+            else if (number % 5 == 0 || number.ToString().Contains("5"))
+            {
+                return "Buzz";
+            }
+            else if (number % 3 == 0 || number.ToString().Contains("3"))
+            {
+                return "Fizz";
+            }
+            else
+            {
+                return number.ToString();
+            }
+        }
+
         static void Main(string[] args)
         {
             string outPath = "..\\..\\..\\..\\FizzBuzz.txt";
             using (StreamWriter sw = new StreamWriter(outPath))
             {
-                for (int i = 0; i <= 300; i++)
+                for (int i = 1; i <= 300; i++)
                 {
-                    sw.WriteLine();
+                    sw.WriteLine($"Result for number:{i} - {GetFizzNumber(i)}");
                 }
-                
+                Console.WriteLine("FizzBuzz.txt has been created");
             }
+            Console.ReadLine();
         }
     }
 }
