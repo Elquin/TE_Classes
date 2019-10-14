431d4265edf6b1f774894f90286b415e583bed2d
Robert Holland <robertrhollandiii@gmail.com>
2019-10-14 17:13:05 -0400

    Post assessment solution building. Working on completing assignment.

--- module-1/Module_1_Coding_Assessment/Mod1Assessment/Mod1Assessment/Models/tellerMachine.cs
+++ module-1/Module_1_Coding_Assessment/Mod1Assessment/Mod1Assessment/Models/tellerMachine.cs
@@ -8,23 +8,40 @@ namespace Mod1Assessment.Models
     {
 
         //constructor
-        public TellerMachine(string manufacturer, decimal deposits, decimal withdrawals)
+        public TellerMachine(string manufacturer, decimal deposits, decimal withdrawals, string cardNumber)
         {
             Manufacturer = manufacturer;
             Deposits = deposits;
             Withdrawals = withdrawals;
+            CardNumber = cardNumber;
         }
 
         //Class properties
         public string Manufacturer { get; set; }
         public decimal Deposits { get; set; }
         public decimal Withdrawals { get; set; }
-       
+        public string CardNumber { get; set; }
 
         public decimal Balance()
         {
             return Deposits - Withdrawals;
         }
 
+        internal bool CardValid(string cardNumber)
+        {
+            if (CardNumber[0].Equals('5') && CardNumber.Length == 16)
+            {
+                return true;
+            }
+            else if (CardNumber[0].Equals('4') && (CardNumber.Length == 13 || CardNumber.Length == 16))
+            {
+                return true;
+            }
+            else if (CardNumber[0].Equals('3') && (CardNumber[1] == 4 || CardNumber[1] == 7))
+            {
+                return true;
+            }
+            else return false;
+        }
     }
 }
--- module-1/Module_1_Coding_Assessment/Mod1Assessment/Mod1Assessment/Program.cs
+++ module-1/Module_1_Coding_Assessment/Mod1Assessment/Mod1Assessment/Program.cs
@@ -1,5 +1,6 @@
 ﻿using Mod1Assessment.Models;
 using System;
+using System.IO;
 
 namespace Mod1Assessment
 {
@@ -10,27 +11,32 @@ namespace Mod1Assessment
             string manufacturer = "Simons";
             decimal deposits = 30.00M;
             decimal withdrawal = 10.00M;
-
-            TellerMachine newTellerMachine = new TellerMachine(manufacturer, deposits, withdrawal)
+            string cardNumber = "5738586394958384";
+            //calling new instance of TellerMachine as newTellerMachine
+            TellerMachine newTellerMachine = new TellerMachine(manufacturer, deposits, withdrawal, cardNumber)
             {
 
             };
+            //Printing out results of methods called from TellerMachine on newTellerMachine.
+            Console.WriteLine(newTellerMachine.Balance());
+            Console.WriteLine(newTellerMachine.CardValid(cardNumber));
+            Console.WriteLine($"ATM - {manufacturer} - {newTellerMachine.Balance()}");
+            //Using StreamReader to read in contents of csv file and assing as string line.
+            int counter = 0;
+            using (StreamReader st = new StreamReader(@"C:\Users\RHolland\git\robertholland-c\module-1\Module_1_Coding_Assessment\Mod1Assessment\Mod1Assessment\Data\TellerInput.csv"))
+            {
+                while (!st.EndOfStream)
+                {
+                    string line = st.ReadLine();
+                    string[] values = line.Split(", ");
 
-            System.Console.WriteLine(newTellerMachine.Balance());
+                }
+            }
+            //
+            
+
+
             Console.ReadLine();
-
-
-            //Started cardValid
-            //string cardNumber = "57385863949583846";
-
-            //bool cardValid(cardNumber)
-            //{
-            //    if (cardNumber[0] == 5 && cardNumber.Length == 16)
-            //    {
-            //        return true;
-            //    }
-            //    else if (cardNumber[0] == 4 && (cardNumber.Length == 13 || cardNumber.Length == 16)
-            //}
 
 
         }
