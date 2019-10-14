aa3cfffddc7e2ee21b709b88632bae22f25db0d5
Robert Holland <robertrhollandiii@gmail.com>
2019-10-14 12:01:23 -0400

    Created TellerMachine Class. Called method for balance in main program.

---
+++ module-1/Module_1_Coding_Assessment/Mod1Assessment/Mod1Assessment/Models/tellerMachine.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Mod1Assessment.Models
+{
+    public class TellerMachine
+    {
+
+        //constructor
+        public TellerMachine(string manufacturer, decimal deposits, decimal withdrawals)
+        {
+            Manufacturer = manufacturer;
+            Deposits = deposits;
+            Withdrawals = withdrawals;
+        }
+
+        //Class properties
+        public string Manufacturer { get; set; }
+        public decimal Deposits { get; set; }
+        public decimal Withdrawals { get; set; }
+        //public decimal Balance
+        //{
+        //    get
+        //    {
+        //        return this.Balance;
+        //    }
+        //    set
+        //    {
+        //        Balance = Deposits - Withdrawals;
+        //    }
+        //}
+
+        public decimal Balance()
+        {
+            return Deposits - Withdrawals;
+        }
+
+    }
+}
--- module-1/Module_1_Coding_Assessment/Mod1Assessment/Mod1Assessment/Program.cs
+++ module-1/Module_1_Coding_Assessment/Mod1Assessment/Mod1Assessment/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Mod1Assessment.Models;
+using System;
 
 namespace Mod1Assessment
 {
@@ -6,8 +7,19 @@ namespace Mod1Assessment
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            string manufacturer = "Simons";
+            decimal deposits = 30.00M;
+            decimal withdrawal = 10.00M;
+
+            TellerMachine newTellerMachine = new TellerMachine(manufacturer, deposits, withdrawal)
+            {
+
+            };
+
+            System.Console.WriteLine(newTellerMachine.Balance());
             Console.ReadLine();
+
+
         }
     }
 }
