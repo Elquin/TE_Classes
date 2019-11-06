b9cf682f3d32dbf06e608c63325918f6be24f23a
Robert Holland <robertrhollandiii@gmail.com>
2019-11-06 15:35:55 -0500

    Almost done with customer search.

--- module-3/06-Forms-and-Controllers-HTTP-GET/student-exercise/GETForms.Web/Controllers/CustomersController.cs
+++ module-3/06-Forms-and-Controllers-HTTP-GET/student-exercise/GETForms.Web/Controllers/CustomersController.cs
@@ -11,6 +11,32 @@ namespace GETForms.Web.Controllers
     public class CustomersController : Controller
     {
 
+        private ICustomerDAO customerDAO;
+        public CustomersController(ICustomerDAO customerDao)
+        {
+            this.customerDAO = customerDao;
+        }
+
+        /// <summary>
+        /// The request to display an empty search page.
+        /// </summary>
+        /// <returns></returns>
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+
+        /// <summary>
+        /// The request to display search results.
+        /// </summary>
+        /// <param name="request">A request model that contains the search parameters.</param>
+        /// <returns></returns>
+        public ActionResult SearchResult(string search, string sortBy)
+        {
+            /* Call the DAL and pass the values as a model back to the View */
+            return View(customerDAO.SearchForCustomers(search, sortBy));
+        }
 
     }
 }
--- module-3/06-Forms-and-Controllers-HTTP-GET/student-exercise/GETForms.Web/DAL/CustomerDAO.cs
+++ module-3/06-Forms-and-Controllers-HTTP-GET/student-exercise/GETForms.Web/DAL/CustomerDAO.cs
@@ -24,7 +24,51 @@ namespace GETForms.Web.DAL
         /// <returns></returns>
         public IList<Customer> SearchForCustomers(string search, string sortBy)
         {
-            throw new NotImplementedException();
+            IList<Customer> customers = new List<Customer>();
+
+            string cmdString;
+            if(sortBy == "Last Name")
+            {
+                cmdString = "SELECT first_name, last_name, email, active FROM customer WHERE last_name LIKE @search OR first_name LIKE @search ORDER BY last_name";
+            }
+            else if(sortBy == "Email")
+            {
+                cmdString = "SELECT first_name, last_name, email, active FROM customer WHERE last_name LIKE @search OR first_name LIKE @search ORDER BY email";
+            }
+            else
+            {
+                cmdString = "SELECT first_name, last_name, email, active FROM customer WHERE last_name LIKE @search OR first_name LIKE @search ORDER BY active";
+            }
+
+            using (SqlConnection conn = new SqlConnection(connectionString))
+            {
+                conn.Open();
+                SqlCommand cmd = new SqlCommand(cmdString, conn);
+                cmd.Parameters.AddWithValue("@search", "%" + search + "%");
+
+                SqlDataReader reader = cmd.ExecuteReader();
+                while (reader.Read())
+                {
+                    customers.Add(MapRowToCustomer(reader));
+                }
+            }
+            return customers;
         }
+
+
+        /// <summary>
+        /// Maps a sql data row to an customer object.
+        /// </summary>
+        /// <param name="reader"></param>
+        /// <returns></returns>
+        private Customer MapRowToCustomer(SqlDataReader reader)
+        {
+            return new Customer()
+            {
+                FirstName = Convert.ToString(reader["first_name"]),
+                LastName = Convert.ToString(reader["last_name"])
+            };
+        }
+
     }
 }
