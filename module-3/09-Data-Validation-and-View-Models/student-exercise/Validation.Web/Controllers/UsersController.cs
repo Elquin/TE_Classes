3f7e56d54da768fd383987ce33e5caccca0051c0
Robert Holland <robertrhollandiii@gmail.com>
2019-11-08 09:05:20 -0500

    Mod 3 Day 9 Solo exercises.

--- module-3/09-Data-Validation-and-View-Models/student-exercise/Validation.Web/Controllers/UsersController.cs
+++ module-3/09-Data-Validation-and-View-Models/student-exercise/Validation.Web/Controllers/UsersController.cs
@@ -12,27 +12,63 @@ namespace Validation.Web.Controllers
     public class UsersController : Controller
     {
         // GET: User
-        public IActionResult Index()
-        {
-            return View("Index");
-        }
+        //public IActionResult Index()   ????
+        //{
+        //    return View("Index");
+        //}
 
         // GET: User/Register
         // Return the empty registration view
+        [HttpGet]
+        public IActionResult Register()
+        {
+            return View();
+        }
 
         // POST: User/Register
         // Validate the model and redirect to confirmation (if successful) or return the 
-        // registration view (if validation fails)        
+        // registration view (if validation fails)    
+        [HttpPost]
+        public IActionResult Register(RegistrationViewModel register)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(register);
+            }
+
+            return RedirectToAction("ConfirmRegister");
+        }
 
         // GET: User/Login
         // Return the empty login view
+        [HttpGet]
+        public IActionResult Login()
+        {
+            return View();
+        }
 
         // POST: User/Login  
         // Validate the model and redirect to login (if successful) or return the 
         // login view (if validation fails)
+        [HttpPost]
+        public IActionResult Login(LoginViewModel user)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(user);
+            }
+
+            return RedirectToAction("ConfirmLogin");
+
+        }
 
         // GET: User/Confirmation
         // Return the confirmation view
+        [HttpGet]
+        public IActionResult ConfirmLogin()
+        {
+            return View();
+        }
 
     }
 }
--- module-3/09-Data-Validation-and-View-Models/student-exercise/Validation.Web/Models/RegistrationViewModel.cs
+++ module-3/09-Data-Validation-and-View-Models/student-exercise/Validation.Web/Models/RegistrationViewModel.cs
@@ -8,6 +8,48 @@ namespace Validation.Web.Models
 {
     public class RegistrationViewModel
     {
-        
+        [Required(ErrorMessage = "Please supply your first name")]
+        [StringLength(20)]
+        public string FirstName { get; set; }
+
+        [Required(ErrorMessage = "Please supply your last name")]
+        [StringLength(20)]
+        public string LastName { get; set; }
+
+        [Required(ErrorMessage = "Please supply your Email")]
+        [EmailAddress(ErrorMessage ="Please supply a valid Email")]
+        public string Email { get; set; }
+
+        //[Required(ErrorMessage = "Please supply your Email")]
+        //[EmailAddress(ErrorMessage = "Please supply a valid Email")]
+        //public string ConfirmEmail { get; set; }
+
+        [Required(ErrorMessage = "Please supply a password")]
+        [StringLength(20, MinimumLength = 8)]
+        public string Password { get; set; }
+
+        //[Required(ErrorMessage = "Please supply a password")]
+        //[StringLength(20, MinimumLength = 8)]
+        //public string ConfirmPassword { get; set; }
+
+        [Required(ErrorMessage = "Please supply your Birth Date")]
+        [DataType(DataType.Date)] 
+        public DateTime BirthDate { get; set; }
+
+        [Required(ErrorMessage = "Please supply a number of tickets")]
+        [Range(1, 10)]
+        public int NumberOfTickets { get; set; }
+
+        public RegistrationViewModel() { }
+
+        public RegistrationViewModel(string firstName, string lastName, string email, string password, DateTime birthDate, int numberOfTickets)
+        {
+            FirstName = firstName;
+            LastName = lastName;
+            Email = email;
+            Password = password;
+            BirthDate = birthDate;
+            NumberOfTickets = numberOfTickets;
+        }
     }
 }
