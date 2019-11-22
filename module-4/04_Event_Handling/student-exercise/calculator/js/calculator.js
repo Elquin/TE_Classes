733b6c81ec851c14c1f5f615496cebfb59ca5fc9
Robert Holland <robertrhollandiii@gmail.com>
2019-11-22 13:41:24 -0500

    Mod 4 Day 4 Completed

--- module-4/04_Event_Handling/student-exercise/calculator/js/calculator.js
+++ module-4/04_Event_Handling/student-exercise/calculator/js/calculator.js
@@ -72,28 +72,56 @@ function clear() {
 }
 
 // add event listener for when the DOM is loaded
-document.addEventListener('LOADED_EVENT_GOES_HERE', () => {
+document.addEventListener('DOMContentLoaded', () => {
 
   // set the variable called display equal to the display element
   // HINT: use its id #display to get a reference to it
+  display = document.getElementById('display');
 
   // get a reference to all of the numbers
   // loop over each of the numbers
   // add a click event listener to each number to call the function clickNumber
+  let numbers = document.getElementsByClassName('number');
+
+  Array.from(numbers).forEach((number) => {
+    number.addEventListener('click', clickNumber );
+  });
 
   // get a reference to the decimal point button
   // add a click event listener to call the function clickNumber
   // the decimal point is part of the number so append it
+  let decimal = document.getElementsByClassName('decimal');
+
+  Array.from(decimal).forEach((dec) => {
+    dec.addEventListener('click', clickNumber );
+  });
 
   // get a reference to the all clear button
-  // add a click event listener to call the function clear  
+  // add a click event listener to call the function clear 
+  
+  let clearBtn = document.getElementsByClassName('all-clear');
 
+  Array.from(clearBtn).forEach((cl) => {
+    cl.addEventListener('click', clear );
+  });
   // get a reference to all of the operators;
   // loop over each of the operators
   // add a click event listener to each operator to call the function clickOperator
+  let operatorBtn = document.getElementsByClassName('operator');
+
+  Array.from(operatorBtn).forEach((op) => {
+    op.addEventListener('click', clickOperator );
+  });
 
   // add click event listener for the equal sign
   // should call the function performOperation
+
+  let equalBtn = document.getElementsByClassName('equal-sign');
+
+  Array.from(equalBtn).forEach((eb) => {
+    eb.addEventListener('click', performOperation );
+  });
+
 
 });
 
