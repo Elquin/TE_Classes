d25c388355f96608a4991f12fa1b9cd6d04f7e89
Robert Holland <robertrhollandiii@gmail.com>
2019-11-26 14:13:35 -0500

    Mod 4 Day 6

---
+++ module-4/06_Creating_APIs/student-exercise/shopping-api-dotnet/ShoppingListAPI/Controllers/ItemController.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using ShoppingListAPI.Models;
+using ShoppingListAPI.Services;
+
+namespace ShoppingListAPI.Controllers
+{
+    [Route("api/groceries")]
+    [ApiController]
+    public class ItemController : ControllerBase
+    {
+        private DataAccessObject dao;
+
+        public ItemController(DataAccessObject dao)
+        {
+            this.dao = dao;
+        }
+
+        /// <summary>
+        /// Returns all items.
+        /// </summary>
+        /// <returns></returns>
+        [HttpGet]
+        public ActionResult<List<Item>> GetAll()
+        {
+            List<Item> items = dao.GetAll();
+            return Ok(items);
+        }
+
+        [HttpGet("{id}", Name = "GetItemById")]
+        public ActionResult<Item> Get(int id)
+        {
+            Item item = dao.Get(id);
+
+            if (item == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(item);
+        }
+
+
+        [HttpPost]
+        public ActionResult<Item> Add([FromBody]Item item)
+        {
+            // Save in the dao
+            dao.Add(item);
+
+            // Return 201 (with a location header: https://localhost:44359/api/reviews/{id})
+            // First parameter - Name of the route to generate for API
+            // Second parameter - The variables for the route. It expects an object. 
+            // Third parameter - The response body
+            return CreatedAtRoute("GetItemById", new { id = item.Id }, item);
+        }
+
+
+        [HttpPut("{id}")]
+        public ActionResult Update(int id, [FromBody]Item item)
+        {
+            Item existingItem = dao.Get(id);
+            if (existingItem == null)
+            {
+                return NotFound();
+            }
+
+            //Copy over the fields we want to update
+            existingItem.name = item.name;
+            existingItem.completed = item.completed;
+
+            //Save existing review back
+            dao.Update(item);
+
+            return NoContent();
+
+        }
+
+
+        /// <summary>
+        /// Deletes an item.
+        /// </summary>
+        [HttpDelete("{id}")]
+        public ActionResult Delete(int id)
+        {
+            Item item = dao.Get(id);
+
+            if (item == null)
+            {
+                return NotFound();
+            }
+
+            dao.Delete(id);
+            return NoContent();
+        }
+
+        
+
+    }
+}
---
+++ module-4/06_Creating_APIs/student-lecture/product-reviews-dotnet/ProductReviewsAPI/Controllers/ProductReviewsController.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using ProductReviewsAPI.Models;
+using ProductReviewsAPI.Services;
+
+namespace ProductReviewsAPI.Controllers
+{
+    [Route("api/reviews")] //Our API is found at http://localhost:xyz/api/reviews
+    [ApiController]
+    public class ProductReviewsController : ControllerBase
+    {
+
+        private DataAccessObject dao;
+
+        public ProductReviewsController(DataAccessObject dao)
+        {
+            this.dao = dao;
+        }
+
+        /// <summary>
+        /// Returns all product reviews.
+        /// </summary>
+        /// <returns></returns>
+        [HttpGet]
+        public ActionResult<List<ProductReview>> GetAll()
+        {
+            List<ProductReview> reviews = dao.GetAll();
+            return Ok(reviews);
+        }
+
+        [HttpGet("{id}", Name = "GetReviewById")]
+        public ActionResult<ProductReview> GetReviewById(int id)
+        {
+            ProductReview review = dao.Get(id);
+
+            if (review == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(review);
+        }
+        /// <summary>
+        /// Deletes a product review.
+        /// </summary>
+        [HttpDelete("{id}")]
+        public ActionResult Delete(int id)
+        {
+            ProductReview review = dao.Get(id);
+
+            if (review == null)
+            {
+                return NotFound();
+            }
+
+            dao.Delete(id);
+            return NoContent();
+        }
+
+        [HttpPost]
+        public ActionResult<ProductReview> CreateNewReview([FromBody]ProductReview productReview)
+        {
+            // Save in the dao
+            dao.Add(productReview);
+
+            // Return 201 (with a location header: https://localhost:44359/api/reviews/{id})
+            // First parameter - Name of the route to generate for API
+            // Second parameter - The variables for the route. It expects an object. We can cover this syntax tomorrow if it is new.
+            // Third parameter - The response body
+            return CreatedAtRoute("GetReviewById", new { id = productReview.Id }, productReview);
+        }
+
+
+        [HttpPut("{id}")]
+        public ActionResult Update(int id, [FromBody]ProductReview productReview)
+        {
+            ProductReview existingReview = dao.Get(id);
+            if(existingReview == null)
+            {
+                return NotFound();
+            }
+
+            //Copy over the fields we want to update
+            existingReview.Name = productReview.Name;
+            existingReview.Title = productReview.Title;
+            existingReview.Review = productReview.Review;
+
+            //Save existing review back
+            dao.Update(existingReview);
+
+            return NoContent();
+
+        }
+
+    }
+}
